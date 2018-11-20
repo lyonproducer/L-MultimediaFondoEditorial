da7fda0f5f27f007b73b4173d43e748420ae74eb
lyonproducer <hernandezleonardo085@gmail.com>
2018-11-20 09:25:46 -0400

    Deploying to Heroku

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -13,11 +13,24 @@ class CORS
      * @param  \Closure  $next
      * @return mixed
      */
+    /*
     public function handle($request, Closure $next)
     {
         header('Access-Control-Allow-Origin : *');
         header('Access-Control-Allow-Headers : Content-type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods : GET, POST, PUT, DELETE, OPTIONS');
         return $next($request);
+    }
+    */
+
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin' , '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+        
+        return $response;    
     }
 }
